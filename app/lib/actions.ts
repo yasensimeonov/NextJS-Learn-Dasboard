4f9eca6958e209d6675f768f8cd2951b823d80a4
Yasen Simeonov <y.simeonov@cnsys.bg>
2024-01-09 18:05:55 +0200

    End of Chapter 13: Handling Errors (error.tsx and not-found.tsx)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -37,10 +37,16 @@ export async function createInvoice(formData: FormData) {
     // console.log(`${customerId}, ${amount}, ${status}, ${amountInCents}, ${date}`);
     // console.log('Customer ID: ' + customerId);
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -55,17 +61,34 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Update Invoice.'
+        };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+    throw new Error('Failed to Delete Invoice.');
+
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return {
+            message: 'Deleted Invoice.'
+        };
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Delete Invoice.'
+        };
+    }
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -158,20 +158,23 @@ export async function fetchInvoiceById(id: string) {
 
   try {
     const data = await sql<InvoiceForm>`
-      SELECT
-        invoices.id,
-        invoices.customer_id,
-        invoices.amount,
-        invoices.status
-      FROM invoices
-      WHERE invoices.id = ${id};
-    `;
+        SELECT
+          invoices.id,
+          invoices.customer_id,
+          invoices.amount,
+          invoices.status
+        FROM invoices
+        WHERE invoices.id = ${id};
+      `;
 
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+
+    // DEBUG ONLY - Invoice is an empty array []
+    // console.log(invoice);
 
     return invoice[0];
   } catch (error) {
